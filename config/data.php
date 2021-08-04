22604b2aa85e22dbedcacc133ac6e8908d89fe5d
Ruben Van Assche <ruben@spatie.be>
2021-08-04 17:27:39 +0200

    Add initial docs

--- config/data.php
+++ config/data.php
@@ -2,20 +2,30 @@
 
 return [
     /*
-     * Transformers will take properties within your data objects and transform
-     * them to types that can be JSON encoded.
+     * The date format to be used when converting a DateTimeInterface from and to json
      */
     'date_format' => DATE_ATOM,
 
+    /*
+     * Transformers will take properties within your data objects and transform
+     * them to types that can be JSON encoded.
+     */
     'transformers' => [
         \Spatie\LaravelData\Transformers\DateTransformer::class,
         \Spatie\LaravelData\Transformers\ArrayableTransformer::class,
     ],
 
+    /*
+     * Global casts will automatically transform values in arrays to data object properties
+     */
     'casts' => [
         DateTimeInterface::class => Spatie\LaravelData\Casts\DateTimeInterfaceCast::class,
     ],
 
+    /*
+     * Validation Rules that will automatically be added when a data object is resolved
+     * from a request
+     */
     'auto_rules' => [
         Spatie\LaravelData\AutoRules\NullableAutoRule::class,
         Spatie\LaravelData\AutoRules\RequiredAutoRule::class,
