47368360d90d72a6c521fe549d80787e5a4c2303
Ruben Van Assche <ruben@spatie.be>
2022-01-31 17:15:48 +0100

    Allow DataCollection::from() to also cast already existing data objects into the correct data objects of the collection

--- src/DataCollection.php
+++ src/DataCollection.php
@@ -171,7 +171,7 @@ class DataCollection implements Responsable, Arrayable, Jsonable, JsonSerializab
 
     protected function ensureAllItemsAreData()
     {
-        $closure = fn ($item) => $item instanceof Data ? $item : $this->dataClass::from($item);
+        $closure = fn ($item) => $item instanceof $this->dataClass ? $item : $this->dataClass::from($item);
 
         $this->items = $this->isPaginated()
             ? $this->items->through($closure)
