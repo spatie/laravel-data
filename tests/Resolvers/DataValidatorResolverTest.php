619f1699184a42341dd647572e9840bad325729b
Alex Manase <malexandru699@gmail.com>
2022-10-27 22:39:21 +0300

    refactor `DataValidatorResolverTest`

--- tests/Resolvers/DataValidatorResolverTest.php
+++ tests/Resolvers/DataValidatorResolverTest.php
@@ -6,29 +6,20 @@ use Spatie\LaravelData\Data;
 use Spatie\LaravelData\Resolvers\DataValidatorResolver;
 use Spatie\LaravelData\Tests\TestCase;
 
-class DataValidatorResolverTest extends TestCase
-{
-    private DataValidatorResolver $resolver;
+beforeEach(function () {
+    $this->resolver = app(DataValidatorResolver::class);
+});
 
-    public function setUp(): void
+it('can set the validator to stop on the first failure', function () {
+    $dataClass = new class() extends Data
     {
-        parent::setUp();
+        public static function stopOnFirstFailure(): bool
+        {
+            return true;
+        }
+    };
 
-        $this->resolver = app(DataValidatorResolver::class);
-    }
+    $validator = $this->resolver->execute($dataClass::class, []);
 
-    /** @test */
-    public function it_can_set_the_validator_to_stop_on_the_first_failure()
-    {
-        $dataClass = new class () extends Data {
-            public static function stopOnFirstFailure(): bool
-            {
-                return true;
-            }
-        };
-
-        $validator = $this->resolver->execute($dataClass::class, []);
-
-        $this->assertTrue(invade($validator)->stopOnFirstFailure);
-    }
-}
+    expect(invade($validator)->stopOnFirstFailure)->toBeTrue();
+});
