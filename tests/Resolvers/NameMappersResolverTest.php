f2b82ed396ba8ad4360604b2ff03c6d7951aea66
Alex Manase <malexandru699@gmail.com>
2022-10-27 23:02:24 +0300

    refactor `NameMappersResolverTest`

--- tests/Resolvers/NameMappersResolverTest.php
+++ tests/Resolvers/NameMappersResolverTest.php
@@ -1,10 +1,6 @@
 <?php
 
-namespace Spatie\LaravelData\Tests\Resolvers;
-
 use Illuminate\Support\Collection;
-use ReflectionAttribute;
-use ReflectionProperty;
 use Spatie\LaravelData\Attributes\MapInputName;
 use Spatie\LaravelData\Attributes\MapName;
 use Spatie\LaravelData\Attributes\MapOutputName;
@@ -12,157 +8,117 @@ use Spatie\LaravelData\Mappers\CamelCaseMapper;
 use Spatie\LaravelData\Mappers\ProvidedNameMapper;
 use Spatie\LaravelData\Mappers\SnakeCaseMapper;
 use Spatie\LaravelData\Resolvers\NameMappersResolver;
-use Spatie\LaravelData\Tests\TestCase;
 
-class NameMappersResolverTest extends TestCase
+function getAttributes(object $class): Collection
 {
-    private NameMappersResolver $resolver;
-
-    public function setUp(): void
-    {
-        parent::setUp();
-
-        $this->resolver = new NameMappersResolver();
-    }
-
-    /** @test */
-    public function it_can_get_an_input_and_output_mapper()
-    {
-        $attributes = $this->getAttributes(new class () {
-            #[MapInputName('input'), MapOutputName('output')]
-            public $property;
-        });
-
-        $this->assertEquals(
-            [
-                'inputNameMapper' => new ProvidedNameMapper('input'),
-                'outputNameMapper' => new ProvidedNameMapper('output'),
-            ],
-            $this->resolver->execute($attributes)
-        );
-    }
-
-    /** @test */
-    public function it_can_have_no_mappers()
-    {
-        $attributes = $this->getAttributes(new class () {
-            public $property;
-        });
-
-        $this->assertEquals(
-            [
-                'inputNameMapper' => null,
-                'outputNameMapper' => null,
-            ],
-            $this->resolver->execute($attributes)
-        );
-    }
-
-    /** @test */
-    public function it_can_have_a_single_map_attribute()
-    {
-        $attributes = $this->getAttributes(new class () {
-            #[MapName('input', 'output')]
-            public $property;
-        });
-
-        $this->assertEquals(
-            [
-                'inputNameMapper' => new ProvidedNameMapper('input'),
-                'outputNameMapper' => new ProvidedNameMapper('output'),
-            ],
-            $this->resolver->execute($attributes)
-        );
-    }
-
-    /** @test */
-    public function it_can_overwrite_a_general_map_attribute()
-    {
-        $attributes = $this->getAttributes(new class () {
-            #[MapName('input', 'output'), MapInputName('input_overwritten')]
-            public $property;
-        });
-
-        $this->assertEquals(
-            [
-                'inputNameMapper' => new ProvidedNameMapper('input_overwritten'),
-                'outputNameMapper' => new ProvidedNameMapper('output'),
-            ],
-            $this->resolver->execute($attributes)
-        );
-    }
-
-    /** @test */
-    public function it_can_map_an_int()
-    {
-        $attributes = $this->getAttributes(new class () {
-            #[MapName(0, 3)]
-            public $property;
-        });
-
-        $this->assertEquals(
-            [
-                'inputNameMapper' => new ProvidedNameMapper(0),
-                'outputNameMapper' => new ProvidedNameMapper(3),
-            ],
-            $this->resolver->execute($attributes)
-        );
-    }
-
-    /** @test */
-    public function it_can_map_a_string()
-    {
-        $attributes = $this->getAttributes(new class () {
-            #[MapName('hello', 'world')]
-            public $property;
-        });
-
-        $this->assertEquals(
-            [
-                'inputNameMapper' => new ProvidedNameMapper('hello'),
-                'outputNameMapper' => new ProvidedNameMapper('world'),
-            ],
-            $this->resolver->execute($attributes)
-        );
-    }
-
-    /** @test */
-    public function it_can_map_a_mapper_class()
-    {
-        $attributes = $this->getAttributes(new class () {
-            #[MapName(CamelCaseMapper::class, SnakeCaseMapper::class)]
-            public $property;
-        });
-
-        $this->assertEquals(
-            [
-                'inputNameMapper' => new CamelCaseMapper(),
-                'outputNameMapper' => new SnakeCaseMapper(),
-            ],
-            $this->resolver->execute($attributes)
-        );
-    }
-
-    /** @test */
-    public function it_can_ignore_certain_mapper_types()
-    {
-        $attributes = $this->getAttributes(new class () {
-            #[MapInputName('input'), MapOutputName(CamelCaseMapper::class)]
-            public $property;
-        });
-
-        $this->assertEquals(
-            [
-                'inputNameMapper' => null,
-                'outputNameMapper' => new CamelCaseMapper(),
-            ],
-            NameMappersResolver::create([ProvidedNameMapper::class])->execute($attributes)
-        );
-    }
-
-    private function getAttributes(object $class): Collection
-    {
-        return collect((new ReflectionProperty($class, 'property'))->getAttributes())
-            ->map(fn (ReflectionAttribute $attribute) => $attribute->newInstance());
-    }
+    return collect((new ReflectionProperty($class, 'property'))->getAttributes())
+        ->map(fn (ReflectionAttribute $attribute) => $attribute->newInstance());
 }
+
+beforeEach(function () {
+    $this->resolver = new NameMappersResolver();
+});
+
+it('can get an input and output mapper', function () {
+    $attributes = getAttributes(new class()
+    {
+        #[MapInputName('input'), MapOutputName('output')]
+        public $property;
+    });
+
+    expect($this->resolver->execute($attributes))->toMatchArray([
+        'inputNameMapper' => new ProvidedNameMapper('input'),
+        'outputNameMapper' => new ProvidedNameMapper('output'),
+    ]);
+});
+
+it('can have no mappers', function () {
+    $attributes = getAttributes(new class()
+    {
+        public $property;
+    });
+
+    expect($this->resolver->execute($attributes))->toMatchArray([
+        'inputNameMapper' => null,
+        'outputNameMapper' => null,
+    ]);
+});
+
+it('can have a single map attribute', function () {
+    $attributes = getAttributes(new class()
+    {
+        #[MapName('input', 'output')]
+        public $property;
+    });
+
+    expect($this->resolver->execute($attributes))->toMatchArray([
+        'inputNameMapper' => new ProvidedNameMapper('input'),
+        'outputNameMapper' => new ProvidedNameMapper('output'),
+    ]);
+});
+
+it('can overwrite a general map attribute', function () {
+    $attributes = getAttributes(new class()
+    {
+        #[MapName('input', 'output'), MapInputName('input_overwritten')]
+        public $property;
+    });
+
+    expect($this->resolver->execute($attributes))->toMatchArray([
+        'inputNameMapper' => new ProvidedNameMapper('input_overwritten'),
+        'outputNameMapper' => new ProvidedNameMapper('output'),
+    ]);
+});
+
+it('can map an int', function () {
+    $attributes = getAttributes(new class()
+    {
+        #[MapName(0, 3)]
+        public $property;
+    });
+
+    expect($this->resolver->execute($attributes))->toMatchArray([
+        'inputNameMapper' => new ProvidedNameMapper(0),
+        'outputNameMapper' => new ProvidedNameMapper(3),
+    ]);
+});
+
+it('can map a string', function () {
+    $attributes = getAttributes(new class()
+    {
+        #[MapName('hello', 'world')]
+        public $property;
+    });
+
+    expect($this->resolver->execute($attributes))->toMatchArray([
+        'inputNameMapper' => new ProvidedNameMapper('hello'),
+        'outputNameMapper' => new ProvidedNameMapper('world'),
+    ]);
+});
+
+it('can map a mapper class', function () {
+    $attributes = getAttributes(new class()
+    {
+        #[MapName(CamelCaseMapper::class, SnakeCaseMapper::class)]
+        public $property;
+    });
+
+    expect($this->resolver->execute($attributes))->toMatchArray([
+        'inputNameMapper' => new CamelCaseMapper(),
+        'outputNameMapper' => new SnakeCaseMapper(),
+    ]);
+});
+
+it('can ignore certain mapper types', function () {
+    $attributes = getAttributes(new class()
+    {
+        #[MapInputName('input'), MapOutputName(CamelCaseMapper::class)]
+        public $property;
+    });
+
+    expect(NameMappersResolver::create([ProvidedNameMapper::class])
+        ->execute($attributes))->toMatchArray([
+        'inputNameMapper' => null,
+        'outputNameMapper' => new CamelCaseMapper(),
+    ]);
+});
