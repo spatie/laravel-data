18af6b2fe2b2b9f8b3e558801e00456e6ba614df
Alex Manase <malexandru699@gmail.com>
2022-10-27 23:08:56 +0300

    refactor `PartialsTreeFromRequestResolverTest`

--- tests/Resolvers/PartialsTreeFromRequestResolverTest.php
+++ tests/Resolvers/PartialsTreeFromRequestResolverTest.php
@@ -17,234 +17,212 @@ use Spatie\LaravelData\Tests\Fakes\LazyData;
 use Spatie\LaravelData\Tests\Fakes\MultiLazyData;
 use Spatie\LaravelData\Tests\TestCase;
 
-class PartialsTreeFromRequestResolverTest extends TestCase
-{
-    private PartialsTreeFromRequestResolver $resolver;
+beforeEach(function () {
+    $this->resolver = resolve(PartialsTreeFromRequestResolver::class);
+});
 
-    public function setUp(): void
+it('will correctly reduce a tree based upon allowed includes', function (
+    ?array $lazyDataAllowedIncludes,
+    ?array $dataAllowedIncludes,
+    ?string $requestedAllowedIncludes,
+    TreeNode $expectedIncludes
+) {
+    LazyData::$allowedIncludes = $lazyDataAllowedIncludes;
+
+    $data = new class(
+        'Hello',
+        LazyData::from('Hello'),
+        LazyData::collection(['Hello', 'World'])
+    ) extends Data
     {
-        parent::setUp();
+        public static ?array $allowedIncludes;
 
-        $this->resolver = resolve(PartialsTreeFromRequestResolver::class);
-    }
-
-    /**
-     * @test
-     * @dataProvider allowedIncludesDataProvider
-     */
-    public function it_will_correctly_reduce_a_tree_based_upon_allowed_includes(
-        ?array $lazyDataAllowedIncludes,
-        ?array $dataAllowedIncludes,
-        ?string $requestedAllowedIncludes,
-        TreeNode $expectedIncludes
-    ) {
-        LazyData::$allowedIncludes = $lazyDataAllowedIncludes;
-
-        $data = new class (
-            'Hello',
-            LazyData::from('Hello'),
-            LazyData::collection(['Hello', 'World'])
-        ) extends Data {
-            public static ?array $allowedIncludes;
-
-            public function __construct(
-                public string $property,
-                public LazyData $nested,
-                #[DataCollectionOf(LazyData::class)]
-                public DataCollection $collection,
-            ) {
-            }
-
-            public static function allowedRequestIncludes(): ?array
-            {
-                return static::$allowedIncludes;
-            }
-        };
-
-        $data::$allowedIncludes = $dataAllowedIncludes;
-
-        $request = request();
-
-        if ($requestedAllowedIncludes !== null) {
-            $request->merge([
-                'include' => $requestedAllowedIncludes,
-            ]);
+        public function __construct(
+            public string $property,
+            public LazyData $nested,
+            #[DataCollectionOf(LazyData::class)]
+            public DataCollection $collection,
+        ) {
         }
 
-        $trees = $this->resolver->execute($data, $request);
+        public static function allowedRequestIncludes(): ?array
+        {
+            return static::$allowedIncludes;
+        }
+    };
 
-        $this->assertEquals(
-            $expectedIncludes,
-            $trees->lazyIncluded
-        );
+    $data::$allowedIncludes = $dataAllowedIncludes;
+
+    $request = request();
+
+    if ($requestedAllowedIncludes !== null) {
+        $request->merge([
+            'include' => $requestedAllowedIncludes,
+        ]);
     }
 
-    public function allowedIncludesDataProvider(): Generator
+    $trees = $this->resolver->execute($data, $request);
+
+    $this->assertEquals(
+        $expectedIncludes,
+        $trees->lazyIncluded
+    );
+})->with(function () {
+    yield 'disallowed property inclusion' => [
+        'lazyDataAllowedIncludes' => [],
+        'dataAllowedIncludes' => [],
+        'requestedIncludes' => 'property',
+        'expectedIncludes' => new ExcludedTreeNode(),
+    ];
+
+    yield 'allowed property inclusion' => [
+        'lazyDataAllowedIncludes' => [],
+        'dataAllowedIncludes' => ['property'],
+        'requestedIncludes' => 'property',
+        'expectedIncludes' => new PartialTreeNode([
+            'property' => new ExcludedTreeNode(),
+        ]),
+    ];
+
+    yield 'allowed data property inclusion without nesting' => [
+        'lazyDataAllowedIncludes' => [],
+        'dataAllowedIncludes' => ['nested'],
+        'requestedIncludes' => 'nested.name',
+        'expectedIncludes' => new PartialTreeNode([
+            'nested' => new ExcludedTreeNode(),
+        ]),
+    ];
+
+    yield 'allowed data property inclusion with nesting' => [
+        'lazyDataAllowedIncludes' => ['name'],
+        'dataAllowedIncludes' => ['nested'],
+        'requestedIncludes' => 'nested.name',
+        'expectedIncludes' => new PartialTreeNode([
+            'nested' => new PartialTreeNode([
+                'name' => new ExcludedTreeNode(),
+            ]),
+        ]),
+    ];
+
+    yield 'allowed data collection property inclusion without nesting' => [
+        'lazyDataAllowedIncludes' => [],
+        'dataAllowedIncludes' => ['collection'],
+        'requestedIncludes' => 'collection.name',
+        'expectedIncludes' => new PartialTreeNode([
+            'collection' => new ExcludedTreeNode(),
+        ]),
+    ];
+
+    yield 'allowed data collection property inclusion with nesting' => [
+        'lazyDataAllowedIncludes' => ['name'],
+        'dataAllowedIncludes' => ['collection'],
+        'requestedIncludes' => 'collection.name',
+        'expectedIncludes' => new PartialTreeNode([
+            'collection' => new PartialTreeNode([
+                'name' => new ExcludedTreeNode(),
+            ]),
+        ]),
+    ];
+
+    yield 'allowed nested data property inclusion without defining allowed includes on nested' => [
+        'lazyDataAllowedIncludes' => null,
+        'dataAllowedIncludes' => ['nested'],
+        'requestedIncludes' => 'nested.name',
+        'expectedIncludes' => new PartialTreeNode([
+            'nested' => new PartialTreeNode([
+                'name' => new ExcludedTreeNode(),
+            ]),
+        ]),
+    ];
+
+    yield 'allowed all nested data property inclusion without defining allowed includes on nested' => [
+        'lazyDataAllowedIncludes' => null,
+        'dataAllowedIncludes' => ['nested'],
+        'requestedIncludes' => 'nested.*',
+        'expectedIncludes' => new PartialTreeNode([
+            'nested' => new AllTreeNode(),
+        ]),
+    ];
+
+    yield 'disallowed all nested data property inclusion ' => [
+        'lazyDataAllowedIncludes' => [],
+        'dataAllowedIncludes' => ['nested'],
+        'requestedIncludes' => 'nested.*',
+        'expectedIncludes' => new PartialTreeNode([
+            'nested' => new ExcludedTreeNode(),
+        ]),
+    ];
+
+    yield 'multi property inclusion' => [
+        'lazyDataAllowedIncludes' => null,
+        'dataAllowedIncludes' => ['nested', 'property'],
+        'requestedIncludes' => 'nested.*,property',
+        'expectedIncludes' => new PartialTreeNode([
+            'property' => new ExcludedTreeNode(),
+            'nested' => new AllTreeNode(),
+        ]),
+    ];
+
+    yield 'without property inclusion' => [
+        'lazyDataAllowedIncludes' => null,
+        'dataAllowedIncludes' => ['nested', 'property'],
+        'requestedIncludes' => null,
+        'expectedIncludes' => new DisabledTreeNode(),
+    ];
+});
+
+it('can combine request and manual includes', function () {
+    $dataclass = new class(
+        Lazy::create(fn () => 'Rick Astley'),
+        Lazy::create(fn () => 'Never gonna give you up'),
+        Lazy::create(fn () => 1986),
+    ) extends MultiLazyData
     {
-        yield 'disallowed property inclusion' => [
-            'lazyDataAllowedIncludes' => [],
-            'dataAllowedIncludes' => [],
-            'requestedIncludes' => 'property',
-            'expectedIncludes' => new ExcludedTreeNode(),
-        ];
+        public static function allowedRequestIncludes(): ?array
+        {
+            return null;
+        }
+    };
 
-        yield 'allowed property inclusion' => [
-            'lazyDataAllowedIncludes' => [],
-            'dataAllowedIncludes' => ['property'],
-            'requestedIncludes' => 'property',
-            'expectedIncludes' => new PartialTreeNode([
-                'property' => new ExcludedTreeNode(),
-            ]),
-        ];
+    $data = $dataclass->include('name')->toResponse(request()->merge([
+        'include' => 'artist',
+    ]))->getData(true);
 
-        yield 'allowed data property inclusion without nesting' => [
-            'lazyDataAllowedIncludes' => [],
-            'dataAllowedIncludes' => ['nested'],
-            'requestedIncludes' => 'nested.name',
-            'expectedIncludes' => new PartialTreeNode([
-                'nested' => new ExcludedTreeNode(),
-            ]),
-        ];
+    $this->assertEquals([
+        'artist' => 'Rick Astley',
+        'name' => 'Never gonna give you up',
+    ], $data);
+});
 
-        yield 'allowed data property inclusion with nesting' => [
-            'lazyDataAllowedIncludes' => ['name'],
-            'dataAllowedIncludes' => ['nested'],
-            'requestedIncludes' => 'nested.name',
-            'expectedIncludes' => new PartialTreeNode([
-                'nested' => new PartialTreeNode([
-                    'name' => new ExcludedTreeNode(),
-                ]),
-            ]),
-        ];
-
-        yield 'allowed data collection property inclusion without nesting' => [
-            'lazyDataAllowedIncludes' => [],
-            'dataAllowedIncludes' => ['collection'],
-            'requestedIncludes' => 'collection.name',
-            'expectedIncludes' => new PartialTreeNode([
-                'collection' => new ExcludedTreeNode(),
-            ]),
-        ];
-
-        yield 'allowed data collection property inclusion with nesting' => [
-            'lazyDataAllowedIncludes' => ['name'],
-            'dataAllowedIncludes' => ['collection'],
-            'requestedIncludes' => 'collection.name',
-            'expectedIncludes' => new PartialTreeNode([
-                'collection' => new PartialTreeNode([
-                    'name' => new ExcludedTreeNode(),
-                ]),
-            ]),
-        ];
-
-        yield 'allowed nested data property inclusion without defining allowed includes on nested' => [
-            'lazyDataAllowedIncludes' => null,
-            'dataAllowedIncludes' => ['nested'],
-            'requestedIncludes' => 'nested.name',
-            'expectedIncludes' => new PartialTreeNode([
-                'nested' => new PartialTreeNode([
-                    'name' => new ExcludedTreeNode(),
-                ]),
-            ]),
-        ];
-
-        yield 'allowed all nested data property inclusion without defining allowed includes on nested' => [
-            'lazyDataAllowedIncludes' => null,
-            'dataAllowedIncludes' => ['nested'],
-            'requestedIncludes' => 'nested.*',
-            'expectedIncludes' => new PartialTreeNode([
-                'nested' => new AllTreeNode(),
-            ]),
-        ];
-
-        yield 'disallowed all nested data property inclusion ' => [
-            'lazyDataAllowedIncludes' => [],
-            'dataAllowedIncludes' => ['nested'],
-            'requestedIncludes' => 'nested.*',
-            'expectedIncludes' => new PartialTreeNode([
-                'nested' => new ExcludedTreeNode(),
-            ]),
-        ];
-
-        yield 'multi property inclusion' => [
-            'lazyDataAllowedIncludes' => null,
-            'dataAllowedIncludes' => ['nested', 'property'],
-            'requestedIncludes' => 'nested.*,property',
-            'expectedIncludes' => new PartialTreeNode([
-                'property' => new ExcludedTreeNode(),
-                'nested' => new AllTreeNode(),
-            ]),
-        ];
-
-        yield 'without property inclusion' => [
-            'lazyDataAllowedIncludes' => null,
-            'dataAllowedIncludes' => ['nested', 'property'],
-            'requestedIncludes' => null,
-            'expectedIncludes' => new DisabledTreeNode(),
-        ];
-    }
-
-    /** @test */
-    public function it_can_combine_request_and_manual_includes()
+it('handles parsing includes from request', function (array $input, array $expected) {
+    $dataclass = new class(
+        Lazy::create(fn () => 'Rick Astley'),
+        Lazy::create(fn () => 'Never gonna give you up'),
+        Lazy::create(fn () => 1986),
+    ) extends MultiLazyData
     {
-        $dataclass = new class (
-            Lazy::create(fn () => 'Rick Astley'),
-            Lazy::create(fn () => 'Never gonna give you up'),
-            Lazy::create(fn () => 1986),
-        ) extends MultiLazyData {
-            public static function allowedRequestIncludes(): ?array
-            {
-                return null;
-            }
-        };
+        public static function allowedRequestIncludes(): ?array
+        {
+            return ['*'];
+        }
+    };
 
-        $data = $dataclass->include('name')->toResponse(request()->merge([
-            'include' => 'artist',
-        ]))->getData(true);
+    $request = request()->merge($input);
 
-        $this->assertEquals([
-            'artist' => 'Rick Astley',
-            'name' => 'Never gonna give you up',
-        ], $data);
-    }
+    $data = $dataclass->toResponse($request)->getData(assoc: true);
 
-    /**
-     * @test
-     * @dataProvider requestInputProvider
-     */
-    public function it_handles_parsing_includes_from_request(array $input, array $expected)
-    {
-        $dataclass = new class (
-            Lazy::create(fn () => 'Rick Astley'),
-            Lazy::create(fn () => 'Never gonna give you up'),
-            Lazy::create(fn () => 1986),
-        ) extends MultiLazyData {
-            public static function allowedRequestIncludes(): ?array
-            {
-                return ['*'];
-            }
-        };
+    $this->assertEquals(
+        array_keys($data),
+        $expected,
+    );
+})->with(function () {
+    yield 'input as array' => [
+        'input' => ['include' => ['artist', 'name']],
+        'expected' => ['artist', 'name'],
+    ];
 
-        $request = request()->merge($input);
-
-        $data = $dataclass->toResponse($request)->getData(assoc: true);
-
-        $this->assertEquals(
-            array_keys($data),
-            $expected,
-        );
-    }
-
-    protected function requestInputProvider()
-    {
-        yield 'input as array' => [
-            'input' => ['include' => ['artist', 'name']],
-            'expected' => ['artist', 'name'],
-        ];
-
-        yield 'input as comma separated' => [
-            'input' => ['include' => 'artist,name'],
-            'expected' => ['artist', 'name'],
-        ];
-    }
-}
+    yield 'input as comma separated' => [
+        'input' => ['include' => 'artist,name'],
+        'expected' => ['artist', 'name'],
+    ];
+});
