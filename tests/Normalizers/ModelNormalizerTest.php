46f5f26f54ce7e3a5c1835853cde240e96c1b3e6
Alex Manase <malexandru699@gmail.com>
2022-10-27 20:53:46 +0300

    refactor `ModelNormalizerTest`

--- tests/Normalizers/ModelNormalizerTest.php
+++ tests/Normalizers/ModelNormalizerTest.php
@@ -12,84 +12,80 @@ use Spatie\LaravelData\Tests\Fakes\Models\FakeModel;
 use Spatie\LaravelData\Tests\Fakes\Models\FakeNestedModel;
 use Spatie\LaravelData\Tests\TestCase;
 
-class ModelNormalizerTest extends TestCase
-{
-    /** @test */
-    public function it_can_get_a_data_object_from_model()
+it('can get a data object from model', function () {
+    $model = FakeModel::factory()->create();
+    $data = FakeModelData::from($model);
+
+    expect($model)
+        ->string->toEqual($data->string)
+        ->nullable->toEqual($data->nullable)
+        ->date->toEqual($data->date);
+});
+
+it('can get a data object with nesting from model and relations', function () {
+    $model = FakeModel::factory()->create();
+
+    $nestedModelA = FakeNestedModel::factory()->for($model)->create();
+    $nestedModelB = FakeNestedModel::factory()->for($model)->create();
+
+    $data = FakeModelData::from($model->load('fakeNestedModels'));
+
+    expect($model)
+        ->string->toEqual($data->string)
+        ->nullable->toEqual($data->nullable)
+        ->date->toEqual($data->date);
+
+    expect($data->fake_nested_models)->toHaveCount(2);
+
+    expect($nestedModelA)
+        ->string->toEqual($data->fake_nested_models[0]->string)
+        ->nullable->toEqual($data->fake_nested_models[0]->nullable)
+        ->date->toEqual($data->fake_nested_models[0]->date)
+        ->and($nestedModelB)
+        ->string->toEqual($data->fake_nested_models[1]->string)
+        ->nullable->toEqual($data->fake_nested_models[1]->nullable)
+        ->date->toEqual($data->fake_nested_models[1]->date);
+});
+
+it('can get a data object from model with dates', function () {
+    $fakeModelClass = new class() extends Model
     {
-        $model = FakeModel::factory()->create();
+        protected $casts = [
+            'date' => 'date',
+            'datetime' => 'datetime',
+            'immutable_date' => 'immutable_date',
+            'immutable_datetime' => 'immutable_datetime',
+        ];
+    };
 
-        $data = FakeModelData::from($model);
+    $model = $fakeModelClass::make([
+        'date' => Carbon::create(2020, 05, 16, 12, 00, 00),
+        'datetime' => Carbon::create(2020, 05, 16, 12, 00, 00),
+        'immutable_date' => Carbon::create(2020, 05, 16, 12, 00, 00),
+        'immutable_datetime' => Carbon::create(2020, 05, 16, 12, 00, 00),
+        'created_at' => Carbon::create(2020, 05, 16, 12, 00, 00),
+        'updated_at' => Carbon::create(2020, 05, 16, 12, 00, 00),
+    ]);
 
-        $this->assertEquals($model->string, $data->string);
-        $this->assertEquals($model->nullable, $data->nullable);
-        $this->assertEquals($model->date, $data->date);
-    }
+    $dataClass = DataBlueprintFactory::new('DataFromModelWithDates')
+        ->withProperty(
+            DataPropertyBlueprintFactory::new('date')->withType(Carbon::class),
+            DataPropertyBlueprintFactory::new('datetime')->withType(Carbon::class),
+            DataPropertyBlueprintFactory::new('immutable_date')->withType(CarbonImmutable::class),
+            DataPropertyBlueprintFactory::new('immutable_datetime')->withType(CarbonImmutable::class),
+            DataPropertyBlueprintFactory::new('created_at')->withType(Carbon::class),
+            DataPropertyBlueprintFactory::new('updated_at')->withType(Carbon::class),
+        )
+        ->create();
 
-    /** @test */
-    public function it_can_get_a_data_object_with_nesting_from_model_and_relations()
-    {
-        $model = FakeModel::factory()->create();
+    $data = $dataClass::from($model);
 
-        $nestedModelA = FakeNestedModel::factory()->for($model)->create();
-        $nestedModelB = FakeNestedModel::factory()->for($model)->create();
-
-        $data = FakeModelData::from($model->load('fakeNestedModels'));
-
-        $this->assertEquals($model->string, $data->string);
-        $this->assertEquals($model->nullable, $data->nullable);
-        $this->assertEquals($model->date, $data->date);
-
-        $this->assertCount(2, $data->fake_nested_models);
-
-        $this->assertEquals($nestedModelA->string, $data->fake_nested_models[0]->string);
-        $this->assertEquals($nestedModelA->nullable, $data->fake_nested_models[0]->nullable);
-        $this->assertEquals($nestedModelA->date, $data->fake_nested_models[0]->date);
-
-        $this->assertEquals($nestedModelB->string, $data->fake_nested_models[1]->string);
-        $this->assertEquals($nestedModelB->nullable, $data->fake_nested_models[1]->nullable);
-        $this->assertEquals($nestedModelB->date, $data->fake_nested_models[1]->date);
-    }
-
-    /** @test */
-    public function it_can_get_a_data_object_from_model_with_dates()
-    {
-        $fakeModelClass = new class () extends Model {
-            protected $casts = [
-                'date' => 'date',
-                'datetime' => 'datetime',
-                'immutable_date' => 'immutable_date',
-                'immutable_datetime' => 'immutable_datetime',
-            ];
-        };
-
-        $model = $fakeModelClass::make([
-            'date' => Carbon::create(2020, 05, 16, 12, 00, 00),
-            'datetime' => Carbon::create(2020, 05, 16, 12, 00, 00),
-            'immutable_date' => Carbon::create(2020, 05, 16, 12, 00, 00),
-            'immutable_datetime' => Carbon::create(2020, 05, 16, 12, 00, 00),
-            'created_at' => Carbon::create(2020, 05, 16, 12, 00, 00),
-            'updated_at' => Carbon::create(2020, 05, 16, 12, 00, 00),
-        ]);
-
-        $dataClass = DataBlueprintFactory::new('DataFromModelWithDates')
-            ->withProperty(
-                DataPropertyBlueprintFactory::new('date')->withType(Carbon::class),
-                DataPropertyBlueprintFactory::new('datetime')->withType(Carbon::class),
-                DataPropertyBlueprintFactory::new('immutable_date')->withType(CarbonImmutable::class),
-                DataPropertyBlueprintFactory::new('immutable_datetime')->withType(CarbonImmutable::class),
-                DataPropertyBlueprintFactory::new('created_at')->withType(Carbon::class),
-                DataPropertyBlueprintFactory::new('updated_at')->withType(Carbon::class),
-            )
-            ->create();
-
-        $data = $dataClass::from($model);
-
-        $this->assertTrue($data->date->eq(Carbon::create(2020, 05, 16, 00, 00, 00)));
-        $this->assertTrue($data->datetime->eq(Carbon::create(2020, 05, 16, 12, 00, 00)));
-        $this->assertTrue($data->immutable_date->eq(Carbon::create(2020, 05, 16, 00, 00, 00)));
-        $this->assertTrue($data->immutable_datetime->eq(Carbon::create(2020, 05, 16, 12, 00, 00)));
-        $this->assertTrue($data->created_at->eq(Carbon::create(2020, 05, 16, 12, 00, 00)));
-        $this->assertTrue($data->updated_at->eq(Carbon::create(2020, 05, 16, 12, 00, 00)));
-    }
-}
+    expect([
+        $data->date->eq(Carbon::create(2020, 05, 16, 00, 00, 00)),
+        $data->datetime->eq(Carbon::create(2020, 05, 16, 12, 00, 00)),
+        $data->immutable_date->eq(Carbon::create(2020, 05, 16, 00, 00, 00)),
+        $data->immutable_datetime->eq(Carbon::create(2020, 05, 16, 12, 00, 00)),
+        $data->created_at->eq(Carbon::create(2020, 05, 16, 12, 00, 00)),
+        $data->updated_at->eq(Carbon::create(2020, 05, 16, 12, 00, 00))
+    ])->each->toBeTrue();
+});
