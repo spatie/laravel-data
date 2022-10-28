a51f1bc5c0c49c8bf42ff06daecf8d2265654a45
Alex Manase <malexandru699@gmail.com>
2022-10-28 12:51:13 +0300

    refactor `PartialTreeNodeTest`

--- tests/Support/TreeNodes/PartialTreeNodeTest.php
+++ tests/Support/TreeNodes/PartialTreeNodeTest.php
@@ -1,86 +1,54 @@
 <?php
 
-namespace Spatie\LaravelData\Tests\Support\TreeNodes;
-
 use Spatie\LaravelData\Support\TreeNodes\AllTreeNode;
 use Spatie\LaravelData\Support\TreeNodes\DisabledTreeNode;
 use Spatie\LaravelData\Support\TreeNodes\ExcludedTreeNode;
 use Spatie\LaravelData\Support\TreeNodes\PartialTreeNode;
-use Spatie\LaravelData\Tests\TestCase;
 
-class PartialTreeNodeTest extends TestCase
-{
-    /** @test */
-    public function it_can_merge_a_node()
-    {
-        $node = new PartialTreeNode([
-            'item' => new ExcludedTreeNode(),
-        ]);
+it('can merge a node', function () {
+    $node = new PartialTreeNode([
+        'item' => new ExcludedTreeNode(),
+    ]);
 
-        $this->assertEquals(
-            new AllTreeNode(),
-            $node->merge(new AllTreeNode())
-        );
-
-        $this->assertEquals(
-            $node,
-            $node->merge(new ExcludedTreeNode())
-        );
-
-        $this->assertEquals(
-            $node,
-            $node->merge(new DisabledTreeNode())
-        );
-
-        $this->assertEquals(
-            $node,
-            $node->merge(new PartialTreeNode())
-        );
-
-        $this->assertEquals(
+    expect($node->merge(new AllTreeNode()))
+        ->toEqual(new AllTreeNode())
+        ->and($node->merge(new ExcludedTreeNode()))
+        ->toEqual($node)
+        ->and($node->merge(new DisabledTreeNode()))
+        ->toEqual($node)
+        ->and($node->merge(new PartialTreeNode()))
+        ->toEqual($node)
+        ->and(
+            $node->merge(new PartialTreeNode(['nested' => new ExcludedTreeNode()]))
+        )
+        ->toEqual(
             new PartialTreeNode([
                 'item' => new ExcludedTreeNode(),
                 'nested' => new ExcludedTreeNode(),
-            ]),
-            $node->merge(new PartialTreeNode(['nested' => new ExcludedTreeNode()]))
+            ])
         );
-    }
+});
 
-    /** @test */
-    public function it_can_intersect_a_node()
-    {
-        $node = new PartialTreeNode([
-            'item' => new ExcludedTreeNode(),
-        ]);
+it('can intersect a node', function () {
+    $node = new PartialTreeNode([
+        'item' => new ExcludedTreeNode(),
+    ]);
 
-        $this->assertEquals(
-            $node,
-            $node->intersect(new AllTreeNode())
-        );
-
-        $this->assertEquals(
-            new ExcludedTreeNode(),
-            $node->intersect(new ExcludedTreeNode())
-        );
-
-        $this->assertEquals(
-            new DisabledTreeNode(),
-            $node->intersect(new DisabledTreeNode())
-        );
-
-        $this->assertEquals(
-            new PartialTreeNode(),
-            $node->intersect(new PartialTreeNode())
-        );
-
-        $this->assertEquals(
-            new PartialTreeNode(),
+    expect($node->intersect(new AllTreeNode()))->toEqual($node)
+        ->and($node->intersect(new ExcludedTreeNode()))
+        ->toEqual(new ExcludedTreeNode())
+        ->and($node->intersect(new DisabledTreeNode()))
+        ->toEqual(new DisabledTreeNode())
+        ->and($node->intersect(new PartialTreeNode()))
+        ->toEqual(new PartialTreeNode())
+        ->and(
             $node->intersect(new PartialTreeNode(['nested' => new ExcludedTreeNode()]))
-        );
-
-        $this->assertEquals(
-            new PartialTreeNode(['item' => new ExcludedTreeNode()]),
+        )
+        ->toEqual(new PartialTreeNode())
+        ->and(
             $node->intersect(new PartialTreeNode(['item' => new ExcludedTreeNode()]))
+        )
+        ->toEqual(
+            new PartialTreeNode(['item' => new ExcludedTreeNode()])
         );
-    }
-}
+});
