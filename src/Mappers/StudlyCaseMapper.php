cc12748b6e008f18e5e2c2353660bdf63cf1eb74
Ruben Van Assche <rubenvanassche@gmail.com>
2022-11-16 17:34:08 +0100

    Add studly mapper and improve docs

---
+++ src/Mappers/StudlyCaseMapper.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Spatie\LaravelData\Mappers;
+
+use Illuminate\Support\Str;
+
+class CamelCaseMapper implements NameMapper
+{
+    public function map(int|string $name): string|int
+    {
+        dump($name);
+
+        if (! is_string($name)) {
+            return $name;
+        }
+
+        return Str::camel($name);
+    }
+}
