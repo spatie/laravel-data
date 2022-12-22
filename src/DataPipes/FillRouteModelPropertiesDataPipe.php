ab50842ee2c4c0639ed84ee6027a552707aa0675
ragulka <illimar.tambek@gmail.com>
2022-12-22 10:06:22 +0200

    Introduce FillRouteModelPropertiesDataPipe

---
+++ src/DataPipes/FillRouteModelPropertiesDataPipe.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Spatie\LaravelData\DataPipes;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
+use Spatie\LaravelData\Attributes\FromRouteModel;
+use Spatie\LaravelData\Support\DataClass;
+
+class FillRouteModelPropertiesDataPipe implements DataPipe
+{
+    public function handle(mixed $payload, DataClass $class, Collection $properties): Collection
+    {
+        if (! $payload instanceof Request) {
+            return $properties;
+        }
+
+        foreach ($class->properties as $dataProperty) {
+            if (! ($attribute = $dataProperty->attributes->first(fn ($attribute) => $attribute instanceof FromRouteModel))) {
+                continue;
+            }
+
+            if (! $attribute->override && $properties->has($dataProperty->name)) {
+                continue;
+            }
+
+            if ($model = $payload->route($attribute->routeModel)) {
+                $routeModelProperty = $attribute->routeModelProperty ?? $dataProperty->name;
+                $properties->put($dataProperty->name, $model->{$routeModelProperty});
+            }
+        }
+
+        return $properties;
+    }
+}
