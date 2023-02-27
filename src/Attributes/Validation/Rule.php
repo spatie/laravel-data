1c960608288f171c05e07fba9ca198f960632edb
EriBloo <19932449+EriBloo@users.noreply.github.com>
2023-02-27 12:02:57 +0100

    Add support for Validation Rule Contract in Rule rule

--- src/Attributes/Validation/Rule.php
+++ src/Attributes/Validation/Rule.php
@@ -5,15 +5,16 @@ namespace Spatie\LaravelData\Attributes\Validation;
 use Attribute;
 use Illuminate\Contracts\Validation\InvokableRule as InvokableRuleContract;
 use Illuminate\Contracts\Validation\Rule as RuleContract;
+use Illuminate\Contracts\Validation\ValidationRule as ValidationRuleContract;
 use Spatie\LaravelData\Support\Validation\ValidationRule;
 
 #[Attribute(Attribute::TARGET_PROPERTY)]
 class Rule extends ValidationRule
 {
-    /** @var array<string|array|ValidationRule|RuleContract|InvokableRuleContract> */
+    /** @var array<string|array|ValidationRule|RuleContract|InvokableRuleContract|ValidationRuleContract> */
     protected array $rules = [];
 
-    public function __construct(string|array|ValidationRule|RuleContract|InvokableRuleContract ...$rules)
+    public function __construct(string|array|ValidationRule|RuleContract|InvokableRuleContract|ValidationRuleContract ...$rules)
     {
         $this->rules = $rules;
     }
