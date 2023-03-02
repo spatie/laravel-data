5b5b6590d341ab1eea35613f6deabd455d550be6
rubenvanassche <rubenvanassche@users.noreply.github.com>
2023-03-02 09:17:32 +0000

    Fix styling

--- tests/Fakes/Castables/SimpleCastable.php
+++ tests/Fakes/Castables/SimpleCastable.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Spatie\LaravelData\Tests\Fakes\Castables;
 
 use Spatie\LaravelData\Casts\Cast;
@@ -7,16 +8,17 @@ use Spatie\LaravelData\Support\DataProperty;
 
 class SimpleCastable implements Castable
 {
-  public function __construct(public string $value) {
+    public function __construct(public string $value)
+    {
+    }
 
-  }
-
-  public static function dataCastUsing(...$arguments): Cast
-  {
-    return new class implements Cast {
-        public function cast(DataProperty $property, mixed $value, array $context): mixed {
-            return new SimpleCastable($value);
-        }
-    };
-  }
+    public static function dataCastUsing(...$arguments): Cast
+    {
+        return new class () implements Cast {
+            public function cast(DataProperty $property, mixed $value, array $context): mixed
+            {
+                return new SimpleCastable($value);
+            }
+        };
+    }
 }
