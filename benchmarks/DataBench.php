2e8ca376b5280a393de18c54e1eed04878d89076
Ruben Van Assche <rubenvanassche@gmail.com>
2023-03-17 10:23:54 +0100

    Better benchmarks

--- benchmarks/DataBench.php
+++ benchmarks/DataBench.php
@@ -1,14 +1,18 @@
 <?php
 
+use Carbon\CarbonImmutable;
 use Illuminate\Support\Collection;
 use Orchestra\Testbench\Concerns\CreatesApplication;
 use PhpBench\Attributes\BeforeMethods;
 use PhpBench\Attributes\Iterations;
 use PhpBench\Attributes\Revs;
 use PhpBench\Benchmark\Metadata\Annotations\Subject;
+use Spatie\LaravelData\DataCollection;
 use Spatie\LaravelData\LaravelDataServiceProvider;
+use Spatie\LaravelData\Optional;
 use Spatie\LaravelData\Tests\Fakes\ComplicatedData;
 use Spatie\LaravelData\Tests\Fakes\MultiNestedData;
+use Spatie\LaravelData\Tests\Fakes\NestedData;
 use Spatie\LaravelData\Tests\Fakes\SimpleData;
 
 class DataBench
@@ -28,7 +32,7 @@ class DataBench
     }
 
     #[Revs(500), Iterations(2)]
-    public function benchData()
+    public function benchDataCreation()
     {
         MultiNestedData::from([
             'nested' => ['simple' => 'Hello'],
@@ -41,7 +45,22 @@ class DataBench
     }
 
     #[Revs(500), Iterations(2)]
-    public function benchDataCollection()
+    public function benchDataTransformation()
+    {
+        $data = new MultiNestedData(
+            new NestedData(new SimpleData('Hello')),
+            new DataCollection(NestedData::class, [
+                new NestedData(new SimpleData('I')),
+                new NestedData(new SimpleData('am')),
+                new NestedData(new SimpleData('groot')),
+            ])
+        );
+
+        $data->toArray();
+    }
+
+    #[Revs(500), Iterations(2)]
+    public function benchDataCollectionCreation()
     {
         $collection = Collection::times(
             15,
@@ -70,5 +89,36 @@ class DataBench
         )->all();
 
         ComplicatedData::collection($collection);
+    }
+
+    #[Revs(500), Iterations(2)]
+    public function benchDataCollectionTransformation()
+    {
+        $collection = Collection::times(
+            15,
+            fn() => new ComplicatedData(
+                42,
+                42,
+                true,
+                3.14,
+                'Hello World',
+                [1, 1, 2, 3, 5, 8],
+                null,
+                Optional::create(),
+                42,
+                CarbonImmutable::create(1994,05,16),
+                new DateTime('1994-05-16T12:00:00+01:00'),
+                new SimpleData('hello'),
+                new DataCollection(NestedData::class, [
+                    new NestedData(new SimpleData('I')),
+                    new NestedData(new SimpleData('am')),
+                    new NestedData(new SimpleData('groot')),
+                ])
+            )
+        )->all();
+
+        $collection = ComplicatedData::collection($collection);
+
+        $collection->toArray();
     }
 }
