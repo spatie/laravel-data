501f5915d7f87db920b76503479d8426aba92e82
mlshvdv <malyshev.co@gmail.com>
2023-03-27 12:55:49 +0300

    Fix usage of properties' mapped output names in 'only', 'except', 'includes', and 'excludes' operations

--- src/Resolvers/PartialsTreeFromRequestResolver.php
+++ src/Resolvers/PartialsTreeFromRequestResolver.php
@@ -7,24 +7,68 @@ use Spatie\LaravelData\Contracts\BaseData;
 use Spatie\LaravelData\Contracts\BaseDataCollectable;
 use Spatie\LaravelData\Contracts\IncludeableData;
 use Spatie\LaravelData\Support\AllowedPartialsParser;
+use Spatie\LaravelData\Support\DataClass;
 use Spatie\LaravelData\Support\DataConfig;
-use Spatie\LaravelData\Support\PartialsParser;
+use Spatie\LaravelData\Support\DataProperty;
 use Spatie\LaravelData\Support\PartialTrees;
+use Spatie\LaravelData\Support\RequestPartialsParser;
 use TypeError;
 
 class PartialsTreeFromRequestResolver
 {
     public function __construct(
         protected DataConfig $dataConfig,
-        protected PartialsParser $partialsParser,
+        protected RequestPartialsParser $partialsParser,
         protected AllowedPartialsParser $allowedPartialsParser,
     ) {
+    }
+
+    protected function createOutputNameMapping(DataClass $dataClass): array
+    {
+        return $dataClass
+            ->properties
+            ->mapWithKeys(function (DataProperty $dataProperty) use ($dataClass) {
+
+                $field = $dataProperty->name;
+                $outputMappedName = $dataProperty->outputMappedName ?? $dataProperty->name;
+
+                if ($dataProperty->type->isDataObject || $dataProperty->type->isDataCollectable) {
+                    return [
+                        $outputMappedName => [
+                            'name' => $field,
+                            'children' => $this->createOutputNameMapping(
+                                $this->dataConfig->getDataClass($dataProperty->type->dataClass)
+                            )
+                        ],
+                    ];
+                }
+
+                return [
+                    $outputMappedName => [
+                        'name' => $field,
+                        'children' => []
+                    ]
+                ];
+            })->all();
     }
 
     public function execute(
         IncludeableData $data,
         Request $request,
     ): PartialTrees {
+
+        $dataClass = match (true) {
+            $data instanceof BaseData => $data::class,
+            $data instanceof BaseDataCollectable => $data->getDataClass(),
+            default => throw new TypeError('Invalid type of data')
+        };
+
+        $dataClass = $this->dataConfig->getDataClass($dataClass);
+
+        $outputNameMapping = $this->createOutputNameMapping($dataClass);
+
+        $this->partialsParser->setOutputNameMapping($outputNameMapping);
+
         $requestedIncludesTree = $this->partialsParser->execute(
             $request->has('include') ? $this->arrayFromRequest($request, 'include') : []
         );
@@ -38,16 +82,10 @@ class PartialsTreeFromRequestResolver
             $request->has('except') ? $this->arrayFromRequest($request, 'except') : []
         );
 
-        $dataClass = match (true) {
-            $data instanceof BaseData => $data::class,
-            $data instanceof BaseDataCollectable => $data->getDataClass(),
-            default => throw new TypeError('Invalid type of data')
-        };
-
-        $allowedRequestIncludesTree = $this->allowedPartialsParser->execute('allowedRequestIncludes', $this->dataConfig->getDataClass($dataClass));
-        $allowedRequestExcludesTree = $this->allowedPartialsParser->execute('allowedRequestExcludes', $this->dataConfig->getDataClass($dataClass));
-        $allowedRequestOnlyTree = $this->allowedPartialsParser->execute('allowedRequestOnly', $this->dataConfig->getDataClass($dataClass));
-        $allowedRequestExceptTree = $this->allowedPartialsParser->execute('allowedRequestExcept', $this->dataConfig->getDataClass($dataClass));
+        $allowedRequestIncludesTree = $this->allowedPartialsParser->execute('allowedRequestIncludes', $dataClass);
+        $allowedRequestExcludesTree = $this->allowedPartialsParser->execute('allowedRequestExcludes', $dataClass);
+        $allowedRequestOnlyTree = $this->allowedPartialsParser->execute('allowedRequestOnly', $dataClass);
+        $allowedRequestExceptTree = $this->allowedPartialsParser->execute('allowedRequestExcept', $dataClass);
 
         $partialTrees = $data->getPartialTrees();
 
---
+++ tests/Fakes/SimpleDataWithMappedOutputName.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Spatie\LaravelData\Tests\Fakes;
+
+use Spatie\LaravelData\Attributes\MapName;
+use Spatie\LaravelData\Attributes\MapOutputName;
+use Spatie\LaravelData\Data;
+use Spatie\LaravelData\Mappers\SnakeCaseMapper;
+
+#[MapName(SnakeCaseMapper::class)]
+class SimpleDataWithMappedOutputName extends Data
+{
+    public function __construct(
+        public int $id,
+        #[MapOutputName('paid_amount')]
+        public float $amount,
+        public string $anyString,
+        public SimpleChildDataWithMappedOutputName $child
+    ) {
+    }
+
+    public static function allowedRequestExcept(): ?array
+    {
+        return [
+            'amount',
+            'anyString',
+            'child'
+        ];
+    }
+}
