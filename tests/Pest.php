3d67bbcaf1e317425b998ac5fc93964ebfd57a4b
Abenet Tamiru <me@abenet.email>
2023-04-01 17:36:42 +0200

    feat: remove superfluous php version check

--- tests/Pest.php
+++ tests/Pest.php
@@ -13,10 +13,3 @@ uses(Spatie\LaravelData\Tests\TestCase::class)->in('.');
 | Functions
 |--------------------------------------------------------------------------
 */
-
-function onlyPHP81()
-{
-    if (version_compare(phpversion(), '8.1', '<')) {
-        test()->markTestIncomplete('This test is only supported in PHP versions > PHP 8.1');
-    }
-}
