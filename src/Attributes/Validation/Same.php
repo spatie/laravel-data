9a69e748d9ee56016e8d3b288b8f70b20cccf712
Ruben Van Assche <rubenvanassche@gmail.com>
2023-05-05 16:02:35 +0200

    Allow field reference in same attribute

--- src/Attributes/Validation/Same.php
+++ src/Attributes/Validation/Same.php
@@ -3,13 +3,17 @@
 namespace Spatie\LaravelData\Attributes\Validation;
 
 use Attribute;
+use Spatie\LaravelData\Support\Validation\References\FieldReference;
 use Spatie\LaravelData\Support\Validation\References\RouteParameterReference;
 
 #[Attribute(Attribute::TARGET_PROPERTY)]
 class Same extends StringValidationAttribute
 {
-    public function __construct(protected string | RouteParameterReference $field)
+    protected FieldReference $field;
+
+    public function __construct(string | FieldReference $field)
     {
+        $this->field = $this->parseFieldReference($field);
     }
 
     public static function keyword(): string
