eeebcf9d46b12542845a7a67b275114396d6a557
Sean OReilly <oreilly.sean@gmail.com>
2023-07-25 16:04:06 +0100

    Added Ulid validation rule. This commit has * an associated test * all tests passing * updated the docs * ulid validation in laravel is documented here https://laravel.com/docs/9.x/validation#rule-ulid

--- src/Support/Validation/ValidationRuleFactory.php
+++ src/Support/Validation/ValidationRuleFactory.php
@@ -80,6 +80,7 @@ use Spatie\LaravelData\Attributes\Validation\StringType;
 use Spatie\LaravelData\Attributes\Validation\Timezone;
 use Spatie\LaravelData\Attributes\Validation\Unique;
 use Spatie\LaravelData\Attributes\Validation\Url;
+use Spatie\LaravelData\Attributes\Validation\Ulid;
 use Spatie\LaravelData\Attributes\Validation\Uuid;
 use Spatie\LaravelData\Exceptions\CouldNotCreateValidationRule;
 
@@ -178,6 +179,7 @@ class ValidationRuleFactory
             Timezone::keyword() => Timezone::class,
             Unique::keyword() => Unique::class,
             Url::keyword() => Url::class,
+            Ulid::keyword() => Ulid::class,
             Uuid::keyword() => Uuid::class,
         ];
     }
