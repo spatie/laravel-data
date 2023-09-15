95105c2fb9ba1872ee26cf918e61306484aebc3c
rubenvanassche <rubenvanassche@users.noreply.github.com>
2023-09-15 09:13:20 +0000

    Fix styling

--- tests/ValidationTest.php
+++ tests/ValidationTest.php
@@ -12,6 +12,10 @@ use Illuminate\Validation\Rules\Exists as LaravelExists;
 use Illuminate\Validation\Rules\In as LaravelIn;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Validation\Validator;
+
+use function Pest\Laravel\mock;
+use function PHPUnit\Framework\assertFalse;
+
 use Spatie\LaravelData\Attributes\DataCollectionOf;
 use Spatie\LaravelData\Attributes\MapInputName;
 use Spatie\LaravelData\Attributes\MapName;
@@ -65,8 +69,6 @@ use Spatie\LaravelData\Tests\Fakes\SimpleDataWithOverwrittenRules;
 use Spatie\LaravelData\Tests\Fakes\Support\FakeInjectable;
 use Spatie\LaravelData\Tests\Fakes\ValidationAttributes\PassThroughCustomValidationAttribute;
 use Spatie\LaravelData\Tests\TestSupport\DataValidationAsserter;
-use function Pest\Laravel\mock;
-use function PHPUnit\Framework\assertFalse;
 
 it('can validate a string', function () {
     $dataClass = new class () extends Data {
@@ -2359,7 +2361,7 @@ it('supports custom validation attributes', function () {
 });
 
 it('can add a requiring rule on an attribute which will overwrite the optional type', function () {
-    $dataClass = new class extends Data {
+    $dataClass = new class () extends Data {
         #[Required]
         #[BooleanType]
         public bool $success;
