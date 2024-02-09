03d4d771f1b5a655a26bc8e505745e75bafbc74b
Ruben Van Assche <rubenvanassche@gmail.com>
2024-02-09 15:29:53 +0100

    Test Husky

--- src/Dto.php
+++ src/Dto.php
@@ -7,7 +7,9 @@ use Spatie\LaravelData\Concerns\ValidateableData;
 use Spatie\LaravelData\Contracts\BaseData as BaseDataContract;
 use Spatie\LaravelData\Contracts\ValidateableData as ValidateableDataContract;
 
-class Dto implements ValidateableDataContract, BaseDataContract
+class Dto implements
+    ValidateableDataContract,
+    BaseDataContract
 {
     use ValidateableData;
     use BaseData;
