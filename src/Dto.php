67d279485fccb78ada54405bd347bf602697c967
Ruben Van Assche <rubenvanassche@gmail.com>
2024-02-09 15:30:04 +0100

    Fix changes

--- src/Dto.php
+++ src/Dto.php
@@ -7,9 +7,7 @@ use Spatie\LaravelData\Concerns\ValidateableData;
 use Spatie\LaravelData\Contracts\BaseData as BaseDataContract;
 use Spatie\LaravelData\Contracts\ValidateableData as ValidateableDataContract;
 
-class Dto implements
-    ValidateableDataContract,
-    BaseDataContract
+class Dto implements ValidateableDataContract, BaseDataContract
 {
     use ValidateableData;
     use BaseData;
