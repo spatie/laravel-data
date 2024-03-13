0bf03cff631a3497ea46f5d670822ed2f61fdc17
Ruben Van Assche <ruben@spatie.be>
2024-03-13 14:57:24 +0100

    Update EnumerableCast.php

--- src/Casts/EnumerableCast.php
+++ src/Casts/EnumerableCast.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Enumerable;
 use Spatie\LaravelData\Support\Creation\CreationContext;
 use Spatie\LaravelData\Support\DataProperty;
 
-/** @deprecated enable the  */
+/** @deprecated enable the iterable casts  */
 class EnumerableCast implements Cast
 {
     public function cast(DataProperty $property, mixed $value, array $properties, CreationContext $context): mixed
