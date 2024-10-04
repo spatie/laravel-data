c62096785117b120880556d0d53bfba901f22aba
Ruben Van Assche <rubenvanassche@gmail.com>
2024-10-04 10:34:01 +0200

    Fix an issue where required rules could not be combined with optional (#844)

--- src/RuleInferrers/AttributesRuleInferrer.php
+++ src/RuleInferrers/AttributesRuleInferrer.php
@@ -3,6 +3,7 @@
 namespace Spatie\LaravelData\RuleInferrers;
 
 use Spatie\LaravelData\Attributes\Validation\Present;
+use Spatie\LaravelData\Attributes\Validation\Sometimes;
 use Spatie\LaravelData\Support\DataProperty;
 use Spatie\LaravelData\Support\Validation\PropertyRules;
 use Spatie\LaravelData\Support\Validation\RequiringRule;
@@ -27,6 +28,10 @@ class AttributesRuleInferrer implements RuleInferrer
             ->each(function (ValidationRule $rule) use ($rules) {
                 if ($rule instanceof Present && $rules->hasType(RequiringRule::class)) {
                     $rules->removeType(RequiringRule::class);
+                }
+
+                if ($rule instanceof RequiringRule && $rules->hasType(Sometimes::class)) {
+                    $rules->removeType(Sometimes::class);
                 }
 
                 $rules->add(
