dcc94275820f6eede19aa66bbe0e1f9d0eabb781
Ruben Van Assche <rubenvanassche@gmail.com>
2025-03-14 11:52:36 +0100

    Fix an issue where the reflectionless structure finder could not find data classes

--- src/Support/Caching/DataClassFinder.php
+++ src/Support/Caching/DataClassFinder.php
@@ -3,6 +3,9 @@
 namespace Spatie\LaravelData\Support\Caching;
 
 use Spatie\LaravelData\Contracts\BaseData;
+use Spatie\LaravelData\Data;
+use Spatie\LaravelData\Dto;
+use Spatie\LaravelData\Resource;
 use Spatie\StructureDiscoverer\Discover;
 
 class DataClassFinder
@@ -10,7 +13,7 @@ class DataClassFinder
     public static function fromConfig(array $config): self
     {
         return new self(
-            directories:  array_filter($config['directories'], 'is_dir'),
+            directories: array_filter($config['directories'], 'is_dir'),
             useReflection: $config['reflection_discovery']['enabled'],
             reflectionBasePath: $config['reflection_discovery']['base_path'],
             reflectionRootNamespace: $config['reflection_discovery']['root_namespace'],
@@ -30,13 +33,24 @@ class DataClassFinder
 
     public function classes(): array
     {
-        $discoverer = Discover::in(...$this->directories)
+        $discoverer = Discover::in(__DIR__.'/../../', ...$this->directories)
             ->implementing(BaseData::class);
 
         if ($this->useReflection) {
             $discoverer->useReflection($this->reflectionBasePath, $this->reflectionRootNamespace);
         }
 
-        return $discoverer->get();
+        $classesToIgnore = [
+            Dto::class,
+            Data::class,
+            Resource::class,
+        ];
+
+        $classes = array_filter(
+            $discoverer->get(),
+            fn (string $class) => ! in_array($class, $classesToIgnore)
+        );
+
+        return array_values($classes);
     }
 }
