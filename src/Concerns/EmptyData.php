8609dc3d9f56e6940e90de7f5424627656690b0d
Thore Sünert <thoresuenert@users.noreply.github.com>
2025-06-12 09:36:02 +0200

    Add the possibibilty to expcet key for empty data generation

--- src/Concerns/EmptyData.php
+++ src/Concerns/EmptyData.php
@@ -6,8 +6,13 @@ use Spatie\LaravelData\Resolvers\EmptyDataResolver;
 
 trait EmptyData
 {
-    public static function empty(array $extra = [], mixed $replaceNullValuesWith = null): array
+    public static function empty(array $extra = [], mixed $replaceNullValuesWith = null, array $except = []): array
     {
-        return app(EmptyDataResolver::class)->execute(static::class, $extra, $replaceNullValuesWith);
+        $emptyData = app(EmptyDataResolver::class)->execute(static::class, $extra, $replaceNullValuesWith);
+        if(count($except) === 0) {
+            return $emptyData;
+        }
+
+        return array_diff_key($emptyData, array_flip($except));
     }
 }
