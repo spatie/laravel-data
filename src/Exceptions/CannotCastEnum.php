8646188523d507903f14b7a791573752cb32cf4b
Ruben Van Assche <rubenvanassche@gmail.com>
2025-06-20 11:06:51 +0200

    Cleanupo

--- src/Exceptions/CannotCastEnum.php
+++ src/Exceptions/CannotCastEnum.php
@@ -3,11 +3,12 @@
 namespace Spatie\LaravelData\Exceptions;
 
 use Exception;
+use Spatie\LaravelData\Support\DataProperty;
 
 class CannotCastEnum extends Exception
 {
-    public static function create(string $type, mixed $value, string $propertyName): self
+    public static function create(string $type, mixed $value, DataProperty $property): self
     {
-        return new self("Could not cast enum (`{$propertyName}`): `{$value}` into a `{$type}`");
+        return new self("Could not cast enum for property {$property->className}::{$property->name} with value `{$value}` into a `{$type}`");
     }
 }
